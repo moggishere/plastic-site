9b64ebf00405666d5d12af492bbe2a7325e0fe03
mogg <aryyoon@gmail.com>
2022-07-31 17:32:55 -0300

    added font

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,40 @@
-<h1>under construction</h1>
+<div class="centralizer">
+	<span class="container">
+		<h1>under construction</h1>
+		<h3>please come back later :)</h3>
+	</span>
+</div>
+
+<style>
+    :global(body) {
+        background-color: antiquewhite;
+        margin: 0 auto;
+    }
+	h1 {
+		font-family: 'Besley', serif;
+		font-weight: 400;
+		margin: 0;
+	}
+	h3 {
+		font-family: 'Work Sans', sans-serif;
+		font-weight: 400;
+	}
+    .centralizer {
+        width: 100vw;
+        height: 100vh;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+	.container {
+		background-color: transparent;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		border: solid 2px;
+		border-radius: 1rem;
+		max-width: 24rem;
+		padding: 1rem;
+	}
+</style>
